239a404e8e07f20c310d100312e3a16c700e3012
Davinia pd <delgadavi@gmail.com>
2024-07-11 15:27:34 +0000

    cambio de diseño completo, añadido y configurado el modal

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// ! inicio funcion generateDomain
+
 function generateDomain() {
   let pronoun = ["the", "our", "your", "their", "my"];
   let adj = ["awful", "bored", "old", "bad", "filthy"];
@@ -29,10 +31,17 @@ function generateDomain() {
   document.getElementById("domain").innerHTML = domainList;
 }
 
+// ! final funcion generateDomain
+
 // window.onload = generateDomain;
+
+// ! click en boton y que habra la funcion
 
 const reload = document.getElementById("reload");
 
 reload.addEventListener("click", generateDomain);
 
+// ! imprimir la frase del titulo y el resultado de la función en la consola
+
 console.log("Choose your favourite grumpy domain");
+console.log(generateDomain);
